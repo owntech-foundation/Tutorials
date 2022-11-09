587c5b627cf7efcbf24a1c296ef4d27871af6f5f
rodgeur (Regis Ruelland) <regis.ruelland@laas.fr>
2022-11-09 11:13:21 +0100

    added current mode and timing functions

--- src/main.cpp
+++ src/main.cpp
@@ -32,12 +32,15 @@
 #include "HardwareConfiguration.h"
 #include "DataAcquisition.h"
 #include "Scheduling.h"
+// #include "CanCommunication.h"
+#include "opalib_pid_current_mode.h"
 #include "opalib_control_pid.h"
 
 //------------ZEPHYR DRIVERS----------------------
 #include "zephyr.h"
+#include "timing/timing.h"
 #include "console/console.h"
-
+#include "drivers/gpio.h"
 
 #define APPLICATION_THREAD_PRIORITY 3
 #define COMMUNICATION_THREAD_PRIORITY 5
@@ -54,59 +57,141 @@ void loop_control_task();       //code to be executed in real-time at 20kHz
 
 enum serial_interface_menu_mode //LIST OF POSSIBLE MODES FOR THE OWNTECH CONVERTER
 {
-    IDLEMODE =0,
+    IDLEMODE = 0,
+    POWERMODE = 1
 };
 
 uint8_t received_serial_char;
 uint8_t mode = IDLEMODE;
 
 //--------------USER VARIABLES DECLARATIONS----------------------
+timing_t start_time, end_time;
+static double kp = 0.1;
+static double ki = 200;
 
+static double voltage_reference = 25;
 
+static double V1_low_value; 
+static double V2_low_value; 
+
+static double meas_data; //temp storage meas value (ctrl task)
 
 //---------------------------------------------------------------
+
+static uint32_t control_task_period = 100; //[us] period of the control task
+static bool pwm_enable = false; //[bool] state of the PWM (ctrl task)
 
 
 //---------------SETUP FUNCTIONS----------------------------------
 
 void setup_hardware()
 {
-    hwConfig.setBoardVersion(SPIN_v_0_9);
-    //setup your hardware here
+    hwConfig.setBoardVersion(TWIST_v_1_1_2);  
+    hwConfig.configureAdcDefaultAllMeasurements();
+    console_init();
+    Init_CurrentMode_peripheral();
+    //Init_CurrentMode_PID(kp, ki, control_task_period);
+    timing_init();
+    console_init();
 }
 
 void setup_software()
 {
+    timing_start();
+    dataAcquisition.start();
+    scheduling.startControlTask(loop_control_task, control_task_period);
+    scheduling.startCommunicationTask(loop_communication_task,COMMUNICATION_THREAD_PRIORITY);
     scheduling.startApplicationTask(loop_application_task,APPLICATION_THREAD_PRIORITY);
-    //setup your software scheduling here
 }
 
 //---------------LOOP FUNCTIONS----------------------------------
 
 void loop_communication_task()
 {
-    while(1) {
-        //communication task code goes here
+     while(1) {
+        received_serial_char = console_getchar();
+        switch (received_serial_char) {
+            case 'h':
+                //----------SERIAL INTERFACE MENU-----------------------
+	        printk(" ________________________________________\n");
+                printk("|     ------- MENU ---------             |\n");
+                printk("|     press i : idle mode                |\n");
+                printk("|     press s : serial mode              |\n");
+                printk("|     press p : power mode               |\n");
+                printk("|     press u : duty cycle UP            |\n");
+                printk("|     press d : duty cycle DOWN          |\n");
+                printk("|________________________________________|\n\n");
+                //------------------------------------------------------
+                break;
+            case 'i':
+                printk("idle mode\n");
+                mode = IDLEMODE;
+                break;
+            case 'p':
+                printk("power mode\n");
+                mode = POWERMODE;
+                break;
+            case 'u':
+                voltage_reference = voltage_reference + 5;
+                printk("up");
+            case 'd' : 
+                voltage_reference = voltage_reference - 5;
+            default:
+                break;
+
+        }
     }
+
 }
+
 
 
 void loop_application_task()
 {
+    uint64_t total_cycles;
+    volatile uint64_t total_ns;
+    uint8_t k;
     while(1){
-
-        printk("Hello World! \n");
-        hwConfig.setLedToggle();  
-        
-        k_msleep(100);    
-    }
-
+        hwConfig.setLedToggle();
+        total_cycles = timing_cycles_get(&start_time, &end_time);
+        total_ns = timing_cycles_to_ns(total_cycles);
+        printk("time = %lld, %lld\n", total_cycles, total_ns);
+        k_msleep(100);
+        }        
 }
-
 
 void loop_control_task()
 {
-    //loop control task code goes here
+    start_time = timing_counter_get();
+    meas_data = dataAcquisition.getV1Low();
+    if (meas_data != -10000)
+        V1_low_value = meas_data;
+
+    meas_data = dataAcquisition.getV2Low();
+    if (meas_data != -10000)
+        V2_low_value = meas_data;
+
+    if (mode == IDLEMODE)
+    {
+        pwm_enable = false;
+        Disable_CurrentMode();
+        // Disable_CurrentMode_leg2();
+    }
+    else if (mode == POWERMODE)
+    {
+
+        if (!pwm_enable)
+        {
+            pwm_enable = true;
+            Enable_CurrentMode();
+            // Enable_CurrentMode_leg2();
+        }
+
+        Update_DutyCycle_CM(voltage_reference, V1_low_value);
+        // Update_DutyCycle_CM_leg2(voltage_reference, V2_low_value);
+    }
+
+    end_time = timing_counter_get();
 }
 
 /**
